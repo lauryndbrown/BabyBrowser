67c5295de5c3953b35d1e8ff33fe2c2dc017b133
lauryndbrown <lauryndbrown@gmail.com>
2017-06-21 16:27:39 -0400

    Added a dom object and created a recursive printing method.

---
+++ baby_browser/html_objects.py
@@ -0,0 +1,56 @@
+class Html_Object:
+    def __init__(self, parent, children, attrs):
+        self.parent = parent
+        if children==None:
+            self.children = []
+        else:
+            self.children = children
+        self.attrs = attrs
+class Tag(Html_Object):
+    def __init__(self, tag, attrs, content=None, parent=None, children=None):
+        super().__init__(parent, children, attrs)
+        self.tag = tag
+        self.content = content
+    def __str__(self):
+        return self.tag
+    def __repr__(self):
+        return self.__str__()
+class DOM:
+    def __init__(self):
+        self.root = None
+        self.current_level = None
+    def add_child(self, html_object):
+        if self.root:
+           self.current_level.children.append(html_object)
+           html_object.parent = self.current_level
+           self.current_level = html_object
+        else:
+            self.root = html_object
+            self.current_level = self.root
+    def close_child(self):
+        self.current_level = self.current_level.parent
+    def __str__(self):
+        return self.str_traverse(self.root, 0)
+    def str_traverse(self, root, level):
+        lst_root = "  "*level+str(root)
+        for child in root.children:
+            lst_root+= "\n"+self.str_traverse(child, level+1)
+        return lst_root
+    def __repr__(self):
+        return self.__str__()
+if __name__=="__main__":
+    html = Tag("html", None)
+    head = Tag("head", None)
+    title = Tag("title", None)
+    body = Tag("body", None)
+    dom = DOM()
+    dom.add_child(html)
+    dom.add_child(head)
+    dom.add_child(title)
+    dom.close_child()#title
+    dom.close_child()#head
+    dom.add_child(body)
+    dom.close_child()#body
+    dom.close_child()#html
+    print(dom)
+
--- baby_browser/html_tokenizer.py
+++ baby_browser/html_tokenizer.py
@@ -1,4 +1,5 @@
 import re
+from baby_browser.html_objects import * 
 #Tokens
 t_OPENTAG = re.compile("<(\w+)>")
 t_CLOSETAG = re.compile("</(\w+)>")
@@ -7,19 +8,21 @@ t_WHITESPACE = re.compile("\s+")
 class Html_Tokenizer:
     def handle_opentag(self, tag, attrs):
         print("Found start tag:", tag)
+        self.dom.add_child(Tag(tag, attrs)) 
     def handle_closetag(self, tag):
         print("Found end tag:", tag)
+        self.dom.close_child() 
     def handle_data(self, data):
         print("Found data:", data)
-
     def p_opentag(self, match):
-        tag = match.group(0)
+        tag = match.group(1)
         return tag, None, len(tag)
     def p_closetag(self, match):
-        tag = match.group(0)
+        tag = match.group(1)
         return tag, None, len(tag)
     def tokenize(self, html):
         index = 0
+        self.dom = DOM()
         while index<len(html):
             index = self.parse(html, index)
     def parse(self, html, index):
@@ -31,11 +34,11 @@ class Html_Tokenizer:
         if opentag:
             tag, attrs, tag_len = self.p_opentag(opentag)
             self.handle_opentag(tag, attrs)
-            add_to_index = tag_len+1
+            add_to_index = tag_len+2
         elif closetag:
             tag, attrs, tag_len = self.p_closetag(closetag)
             self.handle_closetag(tag)
-            add_to_index = tag_len+1
+            add_to_index = tag_len+2
         elif whitespace:
             add_to_index = len(whitespace.group(0))
         elif data:
@@ -48,3 +51,4 @@ if __name__=="__main__":
     html_str = "<html>\n<body>\nHi\n</body>\n</html>"
     tokenizer = Html_Tokenizer()
     tokenizer.tokenize(html_str) 
+    print(tokenizer.dom)
