9e17498e66fa8e3b7b59e24f7ce7b3111de1fdc8
lauryndbrown <lauryndbrown@gmail.com>
2017-06-28 18:30:16 -0400

    got hr to work.

--- baby_browser/qt_html_renderer.py
+++ baby_browser/qt_html_renderer.py
@@ -7,6 +7,7 @@ from baby_browser.networking import *
 class QT_HTML_Renderer:
     HEADERS = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
     IMG = "img"
+    HR = 'hr'
     def __init__(self):
         pass
     def set_page_title(self, title, page):
