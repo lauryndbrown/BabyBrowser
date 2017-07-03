e8834416192c5f6ab6d2a7bc68381407628f149d
lauryndbrown <lauryndbrown@gmail.com>
2017-07-03 11:50:12 -0400

    Added beginnings of unittesting.

---
+++ baby_browser/test/test_html_objects.py
@@ -0,0 +1,7 @@
+import unittest
+from baby_browser.html_objects import *
+from baby_browser.css_objects import *
+
+class TestHtmlObjects(unittest.TestCase):
+    def test_test(self):
+        self.assertEqual(True, True)
