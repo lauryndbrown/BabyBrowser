7c7a1dbc490489f7bbd39afbc7ca7ff4b7841c57
lauryndbrown <lauryndbrown@gmail.com>
2017-07-11 18:03:34 -0500

    created small setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='babybrowser',
+      version='0.1',
+      description='A small browser implementation in Python',
+      url='https://github.com/lauryndbrown/BabyBrowser',
+      author='Lauryn Brown',
+      author_email='lauryndbrown@gmail.com',
+      license='MIT',
+      packages=['baby_browser'],
+      zip_safe=False)
