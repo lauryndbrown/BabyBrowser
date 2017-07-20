521921e8b0742d6eee4f739746c1bb7c0b0152f8
lauryndbrown <lauryndbrown@gmail.com>
2017-07-20 15:20:54 -0500

    Added demo gif

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='babybrowser',
-      version='0.1',
+      version='0.1.1',
       description='A small browser implementation in Python',
       url='https://github.com/lauryndbrown/BabyBrowser',
       author='Lauryn Brown',
